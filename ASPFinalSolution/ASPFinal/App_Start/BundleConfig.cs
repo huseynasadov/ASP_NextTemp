b6ca2463ef73943ccb6d9c93dcf19657bf9092af
huseynasadov <Huseynia@code.edu.az>
2019-11-21 22:18:15 +0400

    init Template Included to ASPFinal Project

---
+++ ASPFinalSolution/ASPFinal/App_Start/BundleConfig.cs
@@ -0,0 +1,24 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace ASPFinal
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/javascript").Include(
+                        "~/Public/js/mdb.min.js",
+                        "~/Public/js/main.js"));
+
+
+            bundles.Add(new StyleBundle("~/bundles/css").Include(
+                      "~/Public/css/mdb.min.css",
+                      "~/Public/css/style.css",
+                      "~/Public/css/responsive.css"));
+
+
+        }
+    }
+}
---
+++ ASPFinalSolution/ASPFinal/Areas/Control/Controllers/AuthController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASPFinal.Areas.Control.Controllers
+{
+    public class AuthController : Controller
+    {
+        // GET: Control/Auth
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
