5f0f247d6511c0b28af70b3e440d7f41f326a329
huseynasadov <Huseynia@code.edu.az>
2019-11-27 22:20:23 +0400

    Setting and pricing CRUD Added

---
+++ ASPFinalSolution/ASPFinal/Areas/Control/Controllers/PricingsController.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ASPFinal.Areas.Control.Filters;
+using ASPFinal.DAL;
+using ASPFinal.Models;
+
+namespace ASPFinal.Areas.Control.Controllers
+{
+    [Auth]
+    public class PricingsController : Controller
+    {
+        private JoobsyDbContext db = new JoobsyDbContext();
+
+        // GET: Control/Pricings
+        public ActionResult Index()
+        {
+            return View(db.Pricings.ToList());
+        }
+
+        // GET: Control/Pricings/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Amount,Bandwidth,Onlinespace,Support,Domain,Fees,OrderBy,Status")] Pricing pricing)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Pricings.Add(pricing);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(pricing);
+        }
+
+        // GET: Control/Pricings/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pricing pricing = db.Pricings.Find(id);
+            if (pricing == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pricing);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Amount,Bandwidth,Onlinespace,Support,Domain,Fees,OrderBy,Status")] Pricing pricing)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(pricing).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(pricing);
+        }
+
+        // GET: Control/Pricings/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pricing pricing = db.Pricings.Find(id);
+            if (pricing == null)
+            {
+                return HttpNotFound();
+            }
+            db.Pricings.Remove(pricing);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ ASPFinalSolution/ASPFinal/Areas/Control/Controllers/SettingsController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ASPFinal.Areas.Control.Filters;
+using ASPFinal.DAL;
+using ASPFinal.Models;
+
+namespace ASPFinal.Areas.Control.Controllers
+{
+    [Auth]
+    public class SettingsController : Controller
+    {
+        private JoobsyDbContext db = new JoobsyDbContext();
+
+        // GET: Control/Settings
+        public ActionResult Index()
+        {
+            return View(db.Settings.ToList());
+        }
+
+
+        // GET: Control/Settings/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Setting setting = db.Settings.Find(id);
+            if (setting == null)
+            {
+                return HttpNotFound();
+            }
+            return View(setting);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,LogoName,Logo,Adress,Email,Phone,Website,Desc,HomeTitle")] Setting setting)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(setting).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(setting);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
